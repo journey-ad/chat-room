1e3f413c3c071ff392122f2e40e58322c036fd46
Anjiurine <q78kg.1ffrj6@outlook.com>
2024-02-25 21:25:33 +0800

    添加基于正则表达式的敏感词过滤

--- utils/filter.js
+++ utils/filter.js
@@ -6,7 +6,6 @@ const scanner = new FastScanner(banwordList)
 
 function filter(word) {
   const list = scanner.search(word)
-
   if(list && list.length > 0){
     list.forEach(item => {
       const re = new RegExp(item[1], "g")
